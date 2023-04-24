ad216f65422a678c1b2dceb00fce9bdc54f03ede
mxtlrr <ruson0914@gmail.com>
2023-04-24 17:30:22 -0500

    backend and v0.1

--- src/thermite.py
+++ src/thermite.py
@@ -2,6 +2,9 @@ from thermite_backend import *
 import PySimpleGUI as sg
 import time
 
+token_read = get_token_from_ini("config.ini")
+print(f"Read token as {token_read}")
+
 layout = [[sg.Text('Message:'), sg.InputText()],
           [sg.Text('ID:'), sg.InputText()],
           [sg.Text('Message 2:'), sg.InputText()],
@@ -11,20 +14,18 @@ layout = [[sg.Text('Message:'), sg.InputText()],
           [sg.Button('Start'), sg.Button('Quit')]]
 
 # Create the Window
-window = sg.Window('Window Title', layout, )
+window = sg.Window('Thermite Client', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
   event, values = window.read()
   if event == sg.WIN_CLOSED or event == 'Quit': break
   if event == 'Start':
+    sg.popup("When you close this popup, we will start.")
     while True:
       # TODO: read token from config.ini
-      send_request(values[1], "foo",
-                  values[0])
+      send_request(values[1], token_read, values[0])
       time.sleep(int(values[3])*60)
-      # Send second message
-      send_request(values[1], "foo", values[2])
-      # Wait again
-      time.sleep(int(values[3])*60)
+      send_request(values[1], token_read, values[2]) # 2nd message
+      time.sleep(int(values[3])*60) # wait
 
 window.close()
--- src/thermite_backend.py
+++ src/thermite_backend.py
@@ -2,5 +2,14 @@
 # Auth: Authentication. In our case, it's user token
 # Content: Content of the message
 def send_request(id: str, auth: str, content: str) -> None:
-  __import__('requests').post(f"https://discord.com/api/v8/channels/{id}/messages",
+  k = __import__('requests').post(f"https://discord.com/api/v8/channels/{id}/messages",
                               data={'content': content}, headers={'authorization': auth})
+  print(f"Response is {k}!")
+  
+
+# Warning! This assumes line 1 is
+# anything with = " followed by your token.
+def get_token_from_ini(file: str) -> str:
+  f = open(file, "rb").readlines()[1].decode()
+  new_f = f[f.find("=")+3:].replace("\"", "")
+  return new_f
